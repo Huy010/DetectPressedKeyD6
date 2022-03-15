dd778a02039ed6f040023e2166a35422a3614049
Huy010 <90165664+Huy010@users.noreply.github.com>
2022-03-15 23:43:14 +0700

    upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var eKey = document.querySelector(".card.key .card__notification");
+var eLocation = document.querySelector(".card.location .card__notification");
+var eWhich = document.querySelector(".card.which .card__notification");
+var eCode = document.querySelector(".card.code .card__notification");
+var result = document.querySelector(".number");
+var alert = document.querySelector(".start");
+var box = document.querySelector(".main");
+
+document.addEventListener("keydown", function (e) {
+  alert.classList.add("hide");
+  box.classList.remove("hide");
+
+  eKey.innerText = e.key;
+  eLocation.innerText = e.location;
+  eWhich.innerText = e.which;
+  eCode.innerText = e.code;
+  result.innerText = e.key;
+});
